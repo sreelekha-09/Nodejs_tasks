285b53e43da0a9445a9dd24179f3fbe81ced613c
sreelekha-09 <59833367+sreelekha-09@users.noreply.github.com>
2020-04-14 18:41:46 +0530

    Update fs-demo.js

--- Task_1/utils/fs-demo.js
+++ Task_1/utils/fs-demo.js
@@ -7,19 +7,24 @@ const curr_path = path.join();
 const status = fs.statSync(curr_path);
 
 
-function hash(curr_path) {
+function calculateHashMd5(curr_path) {
     const result = fs.readFileSync(curr_path, 'utf-8');
-    const hashMD5 = crypto.createHash('md5').update(result).digest('hex');
+    const hashMd5 = crypto.createHash('md5').update(result).digest('hex');
+    return hashMd5;
+
+}
+
+function calculateHashSha1(curr_path) {
+    const result = fs.readFileSync(curr_path, 'utf-8');
     const hashSha1 = crypto.createHash('sha1').update(result).digest('hex');
-    var data = curr_path + "  " + hashSha1 + "  " + hashMD5 + "\n";
+    return hashSha1;
+}
+function getFiles(curr_path) {
+    let mypath = curr_path;
+    var data = mypath + " " + calculateHashSha1(mypath) + "  " + calculateHashMd5(mypath) + "\n";
     fs.appendFile("hashdata.txt", data, function (err) {
         if (err) throw err;
     });
-
-}
-function getRecursiveFiles(curr_path) {
-    let mypath = curr_path;
-    hash(mypath);
     return;
 
 }
@@ -31,25 +36,24 @@ function getRecursiveDir(curr_path) {
         }
         files.forEach(function (file) {
 
-            if (fs.statSync(mypath + '/' + file).isDirectory()) {
+            if (fs.statSync(path.join(mypath + '/' + file)).isDirectory()) {
 
-                getRecursiveDir(mypath + '/' + file);
+                getRecursiveDir(path.join(mypath + '/' + file));
             }
 
             else {
 
-                getRecursiveFiles(mypath + '/' + file);
+                getFiles(path.join(mypath + '/' + file));
 
             }
         });
     });
 }
-if(fs.statSync("./hashdata.txt").isFile())
-        {
-            fs.writeFile('./hashdata.txt', '', function(){console.log('done')})
-        }
+if (fs.statSync("./hashdata.txt").isFile()) {
+    fs.writeFile('./hashdata.txt', '', function () { console.log('done') })
+}
 if (status.isFile()) {
-    getRecursiveFiles(curr_path);
+    getFiles(curr_path);
 }
 if (status.isDirectory()) {
     getRecursiveDir(curr_path);
