d285c1a7aac151b4493b965562eac0d3cff2cc9a
sreelekha-09 <59833367+sreelekha-09@users.noreply.github.com>
2020-04-17 16:53:10 +0530

    changed according to comments

--- Task_1/utils/fs-demo.js
+++ Task_1/utils/fs-demo.js
@@ -3,24 +3,24 @@ const fs = require("fs");
 const crypto = require('crypto');
 const path = require('path');
 
-const curr_path = path.join();
-const status = fs.statSync(curr_path);
+const currPath = path.join();
+const status = fs.statSync(currPath);
 
 
-function calculateHashMd5(curr_path) {
-    const result = fs.readFileSync(curr_path, 'utf-8');
+function calculateHashMd5(currPath) {
+    const result = fs.readFileSync(currPath, 'utf-8');
     const hashMd5 = crypto.createHash('md5').update(result).digest('hex');
     return hashMd5;
 
 }
 
-function calculateHashSha1(curr_path) {
-    const result = fs.readFileSync(curr_path, 'utf-8');
+function calculateHashSha1(currPath) {
+    const result = fs.readFileSync(currPath, 'utf-8');
     const hashSha1 = crypto.createHash('sha1').update(result).digest('hex');
     return hashSha1;
 }
-function getFiles(curr_path) {
-    let mypath = curr_path;
+function getFiles(currPath) {
+    let mypath = currPath;
     var data = mypath + " " + calculateHashSha1(mypath) + "  " + calculateHashMd5(mypath) + "\n";
     fs.appendFile("hashdata.txt", data, function (err) {
         if (err) throw err;
@@ -28,8 +28,8 @@ function getFiles(curr_path) {
     return;
 
 }
-function getRecursiveDir(curr_path) {
-    let mypath = curr_path;
+function getRecursiveDir(currPath) {
+    let mypath = currPath;
     fs.readdir(mypath, function (err, files) {
         if (err) {
             return console.log("error" + err);
@@ -53,9 +53,9 @@ if (fs.statSync("./hashdata.txt").isFile()) {
     fs.writeFile('./hashdata.txt', '', function () { console.log('done') })
 }
 if (status.isFile()) {
-    getFiles(curr_path);
+    getFiles(currPath);
 }
 if (status.isDirectory()) {
-    getRecursiveDir(curr_path);
+    getRecursiveDir(currPath);
 }
 
